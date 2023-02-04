e4753fa02c5a222c933e9590a067ffcad3ea19f9
Walter Czencz <wallyczencz@yahoo.com.ar>
2023-02-04 19:19:56 -0300

    segunda modificación al proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("micro").addEventListener("click",function(){
+    window.open("https://www.google.com.ar/maps/place/Microsoft+Argentina/@-34.5950133,-58.5115825,12z/data=!4m10!1m2!2m1!1smicrosoft!3m6!1s0x95a3353362b5f933:0xc1b70cb49ba27aa0!8m2!3d-34.5950133!4d-58.3715068!15sCgltaWNyb3NvZnQiA4gBAZIBEGNvcnBvcmF0ZV9vZmZpY2XgAQA!16s%2Fg%2F11dyxbk4rs");
+
+})
+document.getElementById("tel1").addEventListener("click",function(){
+    window.open("https://web.whatsapp.com/");
+})
